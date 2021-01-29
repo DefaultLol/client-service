98169f77d3f002f467fe511487f6b1273be4378d
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2021-01-29 23:18:45 +0100

    lol 19

--- src/main/java/com/app/web/ClientController.java
+++ src/main/java/com/app/web/ClientController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/client")
-@CrossOrigin(origins = "*",allowedHeaders = "*")
+@CrossOrigin(origins = "*")
 public class ClientController {
 
     @Autowired
