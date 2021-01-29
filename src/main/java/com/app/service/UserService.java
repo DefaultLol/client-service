1362f0ada3b0ddae523ef6018cb09bf16362d458
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2021-01-29 16:44:45 +0100

    lol 1

--- src/main/java/com/app/service/UserService.java
+++ src/main/java/com/app/service/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-service",url = "https://ensaspay-oauth-service.herokuapp.com/oauthapi/user")
+@FeignClient(name = "user-service",url = "https://ensaspay-oauth-service.herokuapp.com/oauth/api/user")
 public interface UserService {
 
     @PostMapping("/create")
