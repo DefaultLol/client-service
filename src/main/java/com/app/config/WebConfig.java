a32848a41c682c1908a3f1e622c1c9a84a1828d9
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2021-01-29 21:31:20 +0100

    lol 17

---
+++ src/main/java/com/app/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
