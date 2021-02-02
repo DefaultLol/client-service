5778124749f5812672e23f0eccc96040e4a24f76
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2021-02-02 15:23:34 +0100

    create controller  for messagess

--- src/main/java/com/app/AppApplication.java
+++ src/main/java/com/app/AppApplication.java
@@ -1,8 +1,5 @@
 package com.app;
 
-import com.app.twilio.Service;
-import com.app.twilio.SmsRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,8 +9,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableFeignClients
 public class AppApplication {
-    @Autowired
-    private static Service service;
 
     @Bean
     RestTemplate getTemplate(){
@@ -22,12 +17,6 @@ public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
-
-        SmsRequest request=new SmsRequest("+212708042169","Hello first app");
-        service.sendSms(request);
     }
-
-
-
 
 }
--- src/main/java/com/app/twilio/SmsRequest.java
+++ src/main/java/com/app/twilio/SmsRequest.java
@@ -1,15 +1,36 @@
 package com.app.twilio;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 
-@Data @AllArgsConstructor @NoArgsConstructor @ToString
 public class SmsRequest {
-    private String phoneNumber;
-    private String message;
+
+    @NotBlank
+    private final String phoneNumber; // destination
+
+    @NotBlank
+    private final String message;
+
+    public SmsRequest(@JsonProperty("phoneNumber") String phoneNumber,
+                      @JsonProperty("message") String message) {
+        this.phoneNumber = phoneNumber;
+        this.message = message;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "SmsRequest{" +
+                "phoneNumber= ..." + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
